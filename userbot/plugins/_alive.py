bbcd482e9d020ac79552fd40387c3333ef700715
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-09 19:19:37 +0530

    update

--- userbot/plugins/_alive.py
+++ userbot/plugins/_alive.py
@@ -15,12 +15,12 @@ PHANTOM_IMG="https://telegra.ph/file/c7b627350ba5322b6d75d.jpg"
 pm_caption = "**PHANTOM USERBOT IS ONLINE**\n"
 pm_caption += f"**My Master** => {DEFAULTUSER}\n\n"
 pm_caption += "🤖 **SYSTEM INFO** 🤖\n"
-pm_caption += "**ᴛᴇʟᴇᴛʜᴏɴ - ᴠᴇʀsɪᴏɴ ---->**15.0.0\n"
-pm_caption += "**ᴘʏᴛʜᴏɴ -  ᴠᴇʀsɪᴏɴ ------>**3.8.5\n\n"
+pm_caption += "**ᴛᴇʟᴇᴛʜᴏɴ - ᴠᴇʀsɪᴏɴ ----> **15.0.0\n"
+pm_caption += "**ᴘʏᴛʜᴏɴ -  ᴠᴇʀsɪᴏɴ ------> **3.8.5\n\n"
 pm_caption += "**🌀 SUPPORT INFO 🌀**\n"
 pm_caption += "**sᴜᴘᴘᴏʀᴛ - ᴄʜᴀɴɴᴇʟ ---->** [PhantomOt](https://t.me/PhantomOt)\n"
 pm_caption += "**sᴜᴘᴘᴏʀᴛ - ɢʀᴏᴜᴘ =** [PhantomSupport](https://t.me/PhantomSupport)\n\n"
-pm_caption += f"[❤️ Create your own PHANTOM USERBOT ❤️](https://bit.ly/deployphantom)"
+pm_caption += f"**[❤️ Create your own Phantom Userbot ❤️](https://dashboard.heroku.com/new?template=https://github.com/prothinkergang/Phantomuserbot/blob/main)**"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
