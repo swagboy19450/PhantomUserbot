65ea54b0435606eb0fab00c71c57262f1ddebaa6
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-10 19:46:20 +0530

    Update moon.py

--- userbot/plugins/moon.py
+++ userbot/plugins/moon.py
@@ -1,21 +1,14 @@
-"""Emoji
-
-Available Commands:
-
-.emoji shrug
-
-.emoji apple
-
-.emoji :/
-
-.emoji -_-"""
+"""
+avaliable commands :
+.smoon
+.tmoon
+.moon  """
 
 from telethon import events
-
 import asyncio
-
 from uniborg.util import admin_cmd
-
+from userbot.utils import phantom_cmd
+from collections import deque
 
 
 @borg.on(admin_cmd(pattern=r"smoon"))
@@ -57,25 +50,15 @@ async def _(event):
 
 
 @borg.on(admin_cmd(pattern=r"tmoon"))
-
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     animation_interval = 1
-
     animation_ttl = range(0, 33)
-
    # input_str = event.pattern_match.group(1)
-
     #if input_str == "tmoon":
-
     await event.edit("moon")
-
     animation_chars = [
-
             "🌗",
             "🌘",    
             "🌑",
@@ -111,8 +94,17 @@ async def _(event):
         ]
 
     for i in animation_ttl:
-
         await asyncio.sleep(animation_interval)
-
         await event.edit(animation_chars[i % 33])
 
+        
+        
+@borg.on(phantom_cmd(pattern=r"moon", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
