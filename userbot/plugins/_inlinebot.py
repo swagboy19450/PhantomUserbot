5b48b4bc64cfe6e7d0e38f399399f22261ea2f99
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-11 18:21:36 +0530

    hm

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Create Your Own Userbot\nFor More Info Join @PhantomOt"
+            reply_pop_up_alert = "Create Your Own Userbot/nFor More Help, Join @PhantomOt"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,7 +55,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Create Your Own Userbot\nFor More Info Join @PhantomOt"
+            reply_pop_up_alert = "Create Your Own Userbot/nFor More Help, Join @PhantomOt"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
@@ -82,7 +82,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = help_string
         reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-            © Phantom Userbot".format(plugin_name)
+            ©Phantom Userbot".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
         except: 
@@ -90,8 +90,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.answer(halps, cache_time=0, alert=True)
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows ="6"
-    number_of_cols = "2h"
+    number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
+    number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
     multi = Config.EMOJI_TO_DISPLAY_IN_HELP
     helpable_plugins = []
     for p in loaded_plugins:
