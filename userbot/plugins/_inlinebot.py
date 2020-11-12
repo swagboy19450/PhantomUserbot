402f43bf4b0c1f40fd32a63b1e8ef0d2d579a478
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-12 19:04:23 +0530

    hm

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -93,7 +93,6 @@ def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
     multi = Config.EMOJI_TO_DISPLAY_IN_HELP
-    emoticon =random.choice(list(multi))
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
