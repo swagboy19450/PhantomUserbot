aed39832776adeda3bf4241399b0a12f3af9f9d2
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-13 22:22:27 +0530

    hm

--- userbot/plugins/animedp.py
+++ userbot/plugins/animedp.py
@@ -6,7 +6,7 @@ import urllib , os
 from telethon.tl import functions
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
-from userbot.utils import admin_cmd
+from userbot.utils import phantom_cmd
 import asyncio
 from time import sleep
 
