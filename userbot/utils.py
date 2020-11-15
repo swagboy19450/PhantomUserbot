4e6d56f39236f42cca4939db6d7757544fa317ab
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-16 00:40:58 +0530

    @@

--- userbot/utils.py
+++ userbot/utils.py
@@ -459,8 +459,8 @@ def sudo_cmd(pattern=None, **args):
             args["pattern"] = re.compile(pattern)
         else:
             
-            args["pattern"] = re.compile(Config.SUDO_COMMAND_HAND_LER + pattern)
-            reg =Config.SUDO_COMMAND_HAND_LER[1]
+            args["pattern"] = re.compile(Config.SUDO_HNDLR + pattern)
+            reg =Config.SUDO_HNDLR[1]
             cmd = (reg +pattern).replace("$", "").replace("\\", "").replace("^", "")
             try:
                 SUDO_LIST[file_test].append(cmd)
