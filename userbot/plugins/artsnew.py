c1250ea649dfb0d7f4154b26c5b1c6c3aa94625c
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-16 17:24:29 +0530

    adding sudo

--- userbot/plugins/artsnew.py
+++ userbot/plugins/artsnew.py
@@ -3,13 +3,14 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import uname
 from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd, phantom_cmd
+from userbot.utils import sudo_cmd, phantom_cmd
 
 
 n = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 #@command(outgoing=True, pattern="^.ded$")
 @borg.on(phantom_cmd(pattern=r"ded"))
+@borg.on(sudo_cmd(pattern=r"ded", allow_sudo=True))
 async def bluedevilded(ded):
     await ded.edit(n + " ==             |\n　　　　　|" "\n　　　　　| \n"
 "　　　　　| \n"
@@ -79,23 +80,36 @@ E = ("▬▬▬.◙.▬▬▬ \n"
 
 
 @borg.on(phantom_cmd(pattern=r"monster"))
+@borg.on(sudo_cmd(pattern=r"monster", allow_sudo=True))
 async def bluedevilmonster(monster):
     await monster.edit(M)
+    
 @borg.on(phantom_cmd(pattern=r"pig"))
+@borg.on(sudo_cmd(pattern=r"pig", allow_sudo=True))
 async def bluedevilpig(pig):
     await pig.edit(P)
+    
 @borg.on(phantom_cmd(pattern=r"kiler"))
+@borg.on(sudo_cmd(pattern=r"killer", allow_sudo=True))
 async def bluedevilkiller(kiler):
     await kiler.edit(K)
+    
 @borg.on(phantom_cmd(pattern=r"gun"))
+@borg.on(sudo_cmd(pattern=r"gun", allow_sudo=True))
 async def bluedevilgun(gun):
     await gun.edit(G)
+    
 @borg.on(phantom_cmd(pattern=r"dog"))
+@borg.on(sudo_cmd(pattern=r"dog", allow_sudo=True))
 async def bluedevildog(dog):
     await dog.edit(D)    
+    
 @borg.on(phantom_cmd(pattern=r"hello"))
+@borg.on(sudo_cmd(pattern=r"hello", allow_sudo=True))
 async def bluedevilhello(hello):
     await hello.edit(H)
+    
 @borg.on(phantom_cmd(pattern=r"hmf"))
+@borg.on(sudo_cmd(pattern=r"hmf", allow_sudo=True))
 async def bluedevilhmf(hmf):
     await hmf.edit(E)
