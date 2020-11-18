3123858ddc9b075f0e2ba55ce09c4f0758bf7c7f
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-19 00:52:03 +0530

    done ??

--- userbot/plugins/load.py
+++ userbot/plugins/load.py
@@ -1,13 +1,9 @@
 """Emoji
 
 Available Commands:
-
-.load
 .up
-.round
-.heart
-.anim
-.fnl"""
+.gnl"""
+
 import asyncio
 from telethon import events
 from userbot.utils import phantom_cmd, sudo_cmd
@@ -27,37 +23,34 @@ async def _(event):
         await event.edit(animation_chars[i % 4])
     
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(phantom_cmd(pattern="round$", outgoing=True))
+@borg.on(sudo_cmd(pattern="round$", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 0.1
     animation_ttl = range(0, 100)
-    input_str = event.pattern_match.group(1)
-    if input_str == "round":
-        await event.edit(input_str)
-        animation_chars = ["⚫", "⬤", "●", "∘" "‎"]
-        for i in animation_ttl:
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 4])
+    animation_chars = ["⚫", "⬤", "●", "∘" "‎","‎", "●", "⬤","⚫️"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 8])
+        
 
-
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+# @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(phantom_cmd(pattern="gnl$", outgoing=True))
+@borg.on(sudo_cmd(pattern="gnl$", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 2
     animation_ttl = range(0, 6)
-    input_str = event.pattern_match.group(1)
-    if input_str == "gnl":
-        await event.edit(input_str)
-        animation_chars = [
-            "😁🏿",
-            "😁🏾",
-            "😁🏽",
-            "😁🏼",
-            "‎😁",
-            "**Glow & Lovely GeNg Is BeHiNd You....**",
+    animation_chars = [
+        "😁🏿",
+        "😁🏾",
+        "😁🏽",
+        "😁🏼",
+        "‎😁",
+        "**Glow & Lovely GeNg Is BeHiNd You....**",
         ]
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
