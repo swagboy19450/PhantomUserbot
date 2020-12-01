007139001448ac23fb299555283a150dac5cc782
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-12-01 23:02:37 +0530

    ok

--- userbot/plugins/maths.py
+++ userbot/plugins/maths.py
@@ -29,28 +29,28 @@ async def find_tan(event):
 async def find_csc(event):
     input_str = float(event.pattern_match.group(1))
     output = mpmath.csc(input_str)
-    await event.edit(f"**Value of Cosec {input_str}\n==>>**`{output}`") 
+    await event.edit(f"**Value of Cosec {input_str}\n==>>** `{output}`") 
     
 @borg.on(phantom_cmd(pattern="sec ?(.*)"))
 async def find_sec(event):
     input_str = float(event.pattern_match.group(1))
     output = mpmath.sec(input_str)
-    await event.edit(f"**Value of Sec {input_str}\n==>>**`{output}`") 
+    await event.edit(f"**Value of Sec {input_str}\n==>>** `{output}`") 
     
 @borg.on(phantom_cmd(pattern="cot ?(.*)"))
 async def find_cot(event):
     input_str = float(event.pattern_match.group(1))
     output = mpmath.cot(input_str)
-    await event.edit(f"**Value of Cot {input_str}\n==>>**`{output}`") 
+    await event.edit(f"**Value of Cot {input_str}\n==>>** `{output}`") 
 
 @borg.on(phantom_cmd(pattern="sqnum ?(.*)"))
 async def square(event):
     input_str = float(event.pattern_match.group(1))
     output = input_str * input_str
-    await event.edit(f"**Square of {input_str}\n==>>**`{output}`") 
+    await event.edit(f"**Square of {input_str}\n==>>** `{output}`") 
     
 @borg.on(phantom_cmd(pattern="cube?(.*)"))
 async def cube(event):                          
     input_str = float(event.pattern_match.group(1)) #phantomot
     output = input_str * input_str * input_str
-    await event.edit(f"**Cube of {input_str}\n==>>**`{output}`") 
+    await event.edit(f"**Cube of {input_str}\n==>>** `{output}`") 
