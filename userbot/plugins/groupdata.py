7ac41086645d94263cd2deb81fda3f190923ccf2
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-12-01 18:26:37 +0530

    Never Mind

--- userbot/plugins/groupdata.py
+++ userbot/plugins/groupdata.py
@@ -45,10 +45,9 @@ async def leave(e):
         await e.edit(
             f"`My Master is Leaving This CHAT..!!`\n**Reason** - `{input_str}`")
     else:
-        pass
+        await e.edit("`My Master is leaving this chat.....!\nGoodbye..` ")
     await asyncio.sleep(3)
     if "-" in str(e.chat_id):
-        await e.edit("`My Master is leaving this chat.....!\nGoodbye..` ")
         await bot(LeaveChannelRequest(e.chat_id))
     else:
         await e.edit("`Sir This is Not A Chat`")
