614bcf32836e377ef8da32c122b2a65170f28c95
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-12-01 23:53:46 +0530

    Try

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -5,7 +5,7 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import bot, CMD_HELP
-from userbot.utils import phantom_cmd, sudo_cmd
+from userbot.utils import phantom_cmd, sudo_cmd, edit_or_reply
 
 #@register(outgoing=True, pattern="^.q(?: |$)(.*)")
 @borg.on(phantom_cmd(pattern=r"qbot(?: |$)(.*)"))
@@ -14,18 +14,18 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await edit_or_reply(event,"```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
+       await edit_or_reply(event,"```Reply to text message```")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await edit_or_reply(event,"```Reply to actual users message.```")
        return
-    await event.edit("```Making a Quote```")
+    hitler = await edit_or_reply(event,"```Making a Quote```")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
@@ -35,7 +35,7 @@ async def _(event):
               await event.reply("```Please unblock @QuotLyBot and try again```")
               return
           if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+             await hitler.edit("```Can you kindly disable your forward privacy settings for good?```")
           else: 
-             await event.delete()   
+             await hitler.delete()   
              await bot.forward_messages(event.chat_id, response.message)
