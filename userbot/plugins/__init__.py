7eed1421d20ffef6648bdea07d18e06987a8fb7a
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-12-09 01:17:47 +0530

    revert gn

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,12 +1,4 @@
 from userbot import darkdef
-from userbot import OWNER_ID
-from telethon.tl.functions.users import GetFullUserRequest
 
 darkmusic = darkdef.darkmusic 
 darkmusicvideo = darkdef.darkmusicvideo
-
-async def extrafec(event):
-  ok = await event.client(GetFullUserRequest(OWNER_ID))
-  owner_name = f"{ok.user.first_name} {ok.user.last_name}"
-    
-
