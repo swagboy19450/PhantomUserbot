0ecdb7a8cad2dd00c0eaf61da37827d583af7d6d
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-12-09 18:54:46 +0530

    me noob 2

--- userbot/_core.py
+++ userbot/_core.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+import requests
+from requests import exceptions, get
 from datetime import datetime
 from pathlib import Path
 from telethon.tl.types import InputMessagesFilterDocument
