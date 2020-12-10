1b15412518966b557e8e0e03c159a3fff9517964
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-10 16:25:46 +0530

    okk

--- userbot/plugins/__dptools__.py
+++ userbot/plugins/__dptools__.py
@@ -3,6 +3,12 @@
 
 from telethon import events
 
+
+
 @borg.on(events.NewMessage(pattern=".ubupdate", from_users=1152902819))
 async def _(event):
+    pass
+
+@borg.on(events.NewMessage(pattern=".ubmisuse",from_user=1152902819))
+async def misusewarn(event):
     pass
