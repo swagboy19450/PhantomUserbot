e90691c9412ba9234d9b7388aafe1f37798a9343
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-10 16:14:39 +0530

    hmm

--- userbot/plugins/details.py
+++ userbot/plugins/details.py
@@ -1,66 +1,11 @@
-"""Emoji
-
-Available Commands:
-
-.deploy"""
-
 from telethon import events
-
 import asyncio
+from userbot.utils import phantom_cmd, sudo_cmd
+from userbot.utils import edit_or_reply as eor
 
-from uniborg.util import admin_cmd
-from userbot.utils import phantom_cmd, register
-
-from userbot import AUTONAME
-
-
-DEFAULTUSER = str(AUTONAME) if AUTONAME else "Phantom User"
-
-@borg.on(admin_cmd(pattern=r"deploy"))
-
-async def _(event):
-
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 10
-
-    animation_ttl = range(0, 12)
-
-   # input_str = event.pattern_match.group(1)
-
-
-
-    await event.edit("Deploying...")
-
-    animation_chars = [
-        
-            "**Heroku Connecting To Latest Github Build **",
-            "**Build started by user** **{DEFAULTUSER}**",
-            "**Deploy** `535a74f0` **by user** **{MY BOSS}**",
-            "**Restarting Heroku Server...**",
-            "**State changed from up to starting**",    
-            "**Stopping all processes with SIGTERM**",
-            "**Process exited with** `status 143`",
-            "**Starting process with command** `python3 -m stdborg`",
-            "**State changed from starting to up**",
-            "__INFO:UserBot:Logged in as 557667062__",
-            "__INFO:UserBot:Successfully loaded all plugins__",
-            "**Build Succeeded**"
-
- ]
-
-    for i in animation_ttl:
-
-            await asyncio.sleep(animation_interval)
-
-            await event.edit(animation_chars[i % 12])
          
-@register(outgoing=True, pattern="^.repo$")
+@borg.on(phantom_cmd(pattern="repo"))
+@borg.on(phantom_cmd(pattern="repo", allow_sudo = True))
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("**Here is the repo of **[PHANTOM USERBOT ](https://github.com/prothinkergang/phantomuserbot)")
-
-
-            
